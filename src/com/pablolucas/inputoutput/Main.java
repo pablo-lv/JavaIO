28c2ecc2ce638948f0b8634312d229f6192ee424
Pablo Lucas <pablo.lucv@gmail.com>
2016-05-15 20:33:38 -0500

    Created file.txt

--- src/com/pablolucas/inputoutput/Main.java
+++ src/com/pablolucas/inputoutput/Main.java
@@ -1,5 +1,7 @@
 package com.pablolucas.inputoutput;
 
+import java.io.FileWriter;
+
 /**
  * Created by paul on 15/05/16.
  */
@@ -7,5 +9,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String text = "Hello file";
+        String fileName = "MyFile.txt";
+        String route = "/example/route";
+
+        try {
+            FileWriter file = new FileWriter(route + fileName);
+            file.write(text);
+            file.close();
+        } catch (Exception e) {
+
+        }
     }
 }
